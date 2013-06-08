7d31b49b4de66e2d7b545b01e7e3664f8d5be656
anvanza <anthony@vanzandycke.com>
2013-06-08 04:09:50 +0300

    Update invenavi.py

--- invenavi.py
+++ invenavi.py
@@ -16,18 +16,15 @@ class Invenavi:
     parser.add_argument("-d", "--debug", help="increase debugging information output", action='store_true')
     parser.add_argument("-s", "--server", help="server for remote device", default="raspberrypi.local", type=str, action='store')
     parser.add_argument("-dp", "--devport", help="port for device rpc", default=2040, type=int, action='store')
-    parser.add_argument("-cp", "--camport", help="port for camera stream", default=8080, type=int, action='store')
-    
+ 
     # TODO - add further arguments here
     #parser.add_argument(...)        
     
     # and parse
     selected_args = parser.parse_args()
-    self.selected_mode = selected_args.mode
     self.debug = selected_args.debug
     self.config.server_name = selected_args.server
     self.config.rpc_port = selected_args.devport
-    self.config.camera_port = selected_args.camport
         
     #say we're launching
     logging.info("invenavi:\tinvenavi ready to initialize..")
