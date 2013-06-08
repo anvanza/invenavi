b740ab445269c51a5c3af7e637a49c7f6945a1b5
anvanza <anthony@vanzandycke.com>
2013-06-08 18:53:23 +0200

    Update webhost.py

--- web/webhost.py
+++ web/webhost.py
@@ -21,7 +21,7 @@ def run_main_host(kernel, rpc_port):
 
    debug = True
 
-   factory = WebSocketServerFactory("ws://localhost:"+ rpc_port)
+   factory = WebSocketServerFactory("ws://localhost:".format(rpc_port))
 
    factory.protocol = EchoServerProtocol
 
