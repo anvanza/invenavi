a3174c0e4a4f6801dc0da4a90174557159ab9008
anvanza <anthony@vanzandycke.com>
2013-06-08 04:08:51 +0300

    Update invenavi.py

--- invenavi.py
+++ invenavi.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import MySQLdb
+import argparse
 
 from config import invenaviConfig
 from kernel import invenaviKernel
@@ -11,14 +12,25 @@ class Invenavi:
   config = invenaviConfig();
   
   def __init__(self):
+    parser = argparse.ArgumentParser(description='invenavi')
+    parser.add_argument("-d", "--debug", help="increase debugging information output", action='store_true')
+    parser.add_argument("-s", "--server", help="server for remote device", default="raspberrypi.local", type=str, action='store')
+    parser.add_argument("-dp", "--devport", help="port for device rpc", default=2040, type=int, action='store')
+    parser.add_argument("-cp", "--camport", help="port for camera stream", default=8080, type=int, action='store')
     
+    # TODO - add further arguments here
+    #parser.add_argument(...)        
+    
+    # and parse
+    selected_args = parser.parse_args()
+    self.selected_mode = selected_args.mode
+    self.debug = selected_args.debug
+    self.config.server_name = selected_args.server
+    self.config.rpc_port = selected_args.devport
+    self.config.camera_port = selected_args.camport
+        
     #say we're launching
     logging.info("invenavi:\tinvenavi ready to initialize..")
-
-    #self.selected_mode = selected_args.mode
-    self.debug = true
-    self.config.server_name = raspberry.local
-    self.config.rpc_port =2040
     
     db = MySQLdb.connect(host="localhost",user="root",passwd="raspberry", db="test")
     
