e4c66b8a32e3f1e3d188f810c5d9c25c843dc4ce
Anthony Van Zandycke <anthony@vanzandycke.com>
2013-06-17 22:36:09 +0200

    recall

--- external/Adafruit_PWM_Servo_Driver/example.py
+++ external/Adafruit_PWM_Servo_Driver/example.py
@@ -11,8 +11,18 @@ import time
 # bmp = PWM(0x40, debug=True)
 pwm = PWM(0x40, debug=True)
 
-servoMin = 100  # Min pulse length out of 4096
-servoMax = 200  # Max pulse length out of 4096
+servoMin = 150  # Min pulse length out of 4096
+servoMax = 600  # Max pulse length out of 4096
+
+def setServoPulse(channel, pulse):
+  pulseLength = 1000000                   # 1,000,000 us per second
+  pulseLength /= 60                       # 60 Hz
+  print "%d us per period" % pulseLength
+  pulseLength /= 4096                     # 12 bits of resolution
+  print "%d us per bit" % pulseLength
+  pulse *= 1000
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
 
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 while (True):
