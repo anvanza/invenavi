87633660134621a540be809f10139d084833e51b
anvanza <anthony@vanzandycke.com>
2013-06-18 14:57:20 +0300

    Update example.py

--- external/Adafruit_PWM_Servo_Driver/example.py
+++ external/Adafruit_PWM_Servo_Driver/example.py
@@ -11,23 +11,18 @@ import time
 # bmp = PWM(0x40, debug=True)
 pwm = PWM(0x40, debug=True)
 
-servoMin = 150  # Min pulse length out of 4096
-servoMax = 600  # Max pulse length out of 4096
+servoMin = 200  # Min pulse length out of 4096 (1 ms = 200/4095 x 20ms)
+servoNeutral = 300 # neutral pulse length out of 4096 (1.5ms = 300/4095 x 20ms)
+servoMax = 400  # Max pulse length out of 4096 (2ms = 400/4095 x 20ms)
 
-def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
-  pwm.setPWM(channel, 0, pulse)
-
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+pwm.setPWMFreq(50)  # Set frequency to 50 Hz (servo pulse period = 20ms)
 while (True):
-  # Change speed of continuous servo on channel O
-  pwm.setPWM(0, 0, servoMin)
-  time.sleep(1)
-  pwm.setPWM(0, 0, servoMax)
-  time.sleep(1)
+	# Change speed of continuous servo on channel O
+	pwm.setPWM(0, 0, servoMin)
+	time.sleep(1)
+	pwm.setPWM(0, 0, servoNeutral)
+	time.sleep(1)
+	pwm.setPWM(0, 0, servoMax)
+	time.sleep(1)
+	pwm.setPWM(0, 0, servoNeutral)
+	time.sleep(1)
