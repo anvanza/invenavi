b10a782a63ce487ce54aac670fb0f2d26d0ec01a
Anthony Van Zandycke <anthony@vanzandycke.com>
2013-08-31 17:45:18 +0200

    test2

--- config.py
+++ config.py
@@ -126,7 +126,7 @@ class invenaviConfig(object):
             try:
                 from vehicle.drive_controller import AdafruitDriveController
                 # TODO pwm addresses from config?
-                self.drive_controller = AdafruitDriveController(i2c_addr=addr, 1, debug=debug)
+                self.drive_controller = AdafruitDriveController(i2c_addr=addr, i2c_bus=1, debug=debug)
             except Exception as ex:
                 logging.info("CFG:\tError setting up DRIVECONTROLLER over i2c - %s" % ex)
                 self.drive_controller = DummyDriveController()
