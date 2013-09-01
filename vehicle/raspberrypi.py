c059ff5996992da9597dd77ebd4152c5242e94a4
Anthony Van Zandycke <anthony@vanzandycke.com>
2013-09-01 20:29:31 +0200

    addraspberry

---
+++ vehicle/raspberrypi.py
@@ -0,0 +1,13 @@
+import os
+import smbus
+import subprocess
+
+def i2c_bus():
+    return smbus.SMBus(1)
+
+def i2c_bus_num():
+    return '1'
+
+def serial_bus():
+    return "/dev/ttyUSB0"
+    #return "/dev/ttyAMA0"
