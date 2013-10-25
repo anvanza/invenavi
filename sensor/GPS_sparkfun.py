6fe9b6a24ea822c5c515d7530bbed751826557b9
Anthony Van Zandycke <anthony@vanzandycke.com>
2013-10-25 18:21:55 +0200

    it's linux

--- sensor/GPS_sparkfun.py
+++ sensor/GPS_sparkfun.py
@@ -176,16 +176,7 @@ def save_raw():
 				stream_serial()
 
 def scan():
-    #scan for available ports. return a list of tuples (num, name)
-    available = []
-    for i in range(256):
-        try:
-            s = serial.Serial(i)
-            available.append( (i, s.name))
-            s.close()   # explicit close 'cause of delayed GC in java
-        except serial.SerialException:
-            pass
-    return available
+    return glob.glob('/dev/ttyS*')
 
 def stream_serial():
     #stream data directly from the serial port
