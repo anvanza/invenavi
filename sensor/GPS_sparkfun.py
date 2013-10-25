d61c5781ba1be6135e8c450732d8ab655d8fc1fe
Anthony Van Zandycke <anthony@vanzandycke.com>
2013-10-25 18:40:01 +0200

    baudrate

--- sensor/GPS_sparkfun.py
+++ sensor/GPS_sparkfun.py
@@ -39,7 +39,7 @@ TRX = -105.1621          #top right longitude
 TRY = 40.0868            #top right latitude
 BLX = -105.2898          #bottom left longitude
 BLY = 40.001             #bottom left latitude
-BAUDRATE = 4800
+BAUDRATE = 9600
 lat_input = 0            #latitude of home marker
 long_input = 0           #longitude of home marker
 
@@ -160,7 +160,6 @@ def save_raw():
 	while 1:
 		line = ser.readline()
 		line_str = str(line)
-		print line_str
 		if(line_str[4] == 'G'): # $GPGGA
 			if(len(line_str) > 50):
 				# open txt file and log data
