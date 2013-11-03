abcd5608d2a2e520261640c427b1f4f210e3ad13
Anthony Van Zandycke <anthony@vanzandycke.com>
2013-11-03 15:47:30 +0100

    more

--- sensor/GPS_sparkfun.py
+++ sensor/GPS_sparkfun.py
@@ -55,46 +55,7 @@ def init_serial():
 	ser.open()
 	ser.isOpen()
 
-	thread()
-
-def position():
-	#opens a the saved txt file, parses for lat and long, displays on map
-	global lat, long, lat_input, long_input, pos_x, pos_y, altitude
-	global BLX, BLY, TRX, TRY
-
-	#same process here as in altitude
-	f1 = open('temp.txt', 'w')
-	f1.truncate()
-	shutil.copyfile('nmea.txt', 'temp.txt')
-	f1.close()
-
-	f1 = open('temp.txt', 'r') #open and read only
-	try:
-		for line in f1:
-			if(line[4] == 'G'): # $GPGGA
-				if(len(line) > 50):
-					#print line
-					gpgga = nmea.GPGGA()
-					gpgga.parse(line)
-					lats = gpgga.latitude
-					longs = gpgga.longitude
-
-					#convert degrees,decimal minutes to decimal degrees
-					lat1 = (float(lats[2]+lats[3]+lats[4]+lats[5]+lats[6]+lats[7]+lats[8]))/60
-					lat = (float(lats[0]+lats[1])+lat1)
-					long1 = (float(longs[3]+longs[4]+longs[5]+longs[6]+longs[7]+longs[8]+longs[9]))/60
-					long = (float(longs[0]+longs[1]+longs[2])+long1)
-
-					#calc position
-					pos_y = lat
-					pos_x = -long #longitude is negaitve
-
-
-					#shows that we are reading through this loop
-					print pos_x
-					print pos_y
-	finally:
-		f1.close()
+	save_raw()
 
 def save_raw():
 	#this fxn creates a txt file and saves only GPGGA sentences
@@ -127,6 +88,5 @@ def save_raw():
 
 ########START#####################################################################################
 init_serial()
-position()
 ser.close()
 sys.exit()
