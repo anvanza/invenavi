6f44adb4ab0c19e0f808e0ef8f81047f1cd89d34
Anthony Van Zandycke <anthony.vanzandycke@phpro.be>
2014-08-24 17:13:00 +0200

    edit test

--- tests/LSM303_test.py
+++ tests/LSM303_test.py
@@ -1,11 +1,17 @@
 import sys
+import time
+
 sys.path.append("/home/pi/invenavi")
 from sensor.Adafruit_LSM303DLHC import LSM303DLHC
 
-debug = True
+debug = False
 
 compass_sensor = LSM303DLHC(0x19, 0x1E, debug)
 compass_sensor.set_temp_enabled(True)
+compass_sensor.set_accelerometer_low_power_mode(False)
+compass_sensor.set_accelerometer_high_resolution(True)
+
 
 while True:
     print compass_sensor.read_magnetic_heading()
+    time.sleep(1000)
