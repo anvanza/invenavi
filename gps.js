84fd4f6ec29f895265a1ef73698bde73f5564ff9
Anthony Van Zandycke <anthony.vanzandycke@phpro.be>
2015-10-24 21:19:18 +0200

    Add own package

--- gps.js
+++ gps.js
@@ -5,27 +5,34 @@ var Gps = function (kernel) {
   this.start = function(level) {
     console.log("Starting GPS");
     var gpsy = require("gpsy");
-    var gps = gpsy("/dev/ttyAMA0"); // your serial device
+    var gps = gpsy("/dev/ttyAMA0", 9600); // your serial device
 
     gps.on("fix", function(ring){
+      console.log('fix');
       console.log(ring);
     });
     gps.on("position", function(ring){
+      console.log("position");
       console.log(ring);
     });
     gps.on("speed", function(ring){
+      console.log("speed");
       console.log(ring);
     });
-    gps.on("time",function(ring){
+    gps.on("time", function(ring){
+      console.log("time");
       console.log(ring);
     });
-    gps.on("data",function(ring){
+    gps.on("data", function(ring){
+      console.log("data");
       console.log(ring);
     });
-    gps.on("error",function(ring){
+    gps.on("err", function(ring){
+      console.log("error");
       console.log(ring);
     });
-    gps.on("close",function(ring){
+    gps.on("close", function(ring){
+      console.log("close");
       console.log(ring);
     });
 
